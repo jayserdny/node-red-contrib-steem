0780040ff6884db9e1a914dcf36058362fe6e1fa
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-01-28 04:42:55 -0500

    Globals nodes added

---
+++ nodes/globals/getNextScheduledHardfork.js
@@ -0,0 +1,36 @@
+var steem = require('steem');
+
+module.exports = (RED) => {
+    "use strict";
+    function getNextScheduledHardforkNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        
+        node.on('input', (msg) => {
+
+            // Set initial status of the node
+            node.status({});
+
+            steem.api.getNextScheduledHardfork((err, response) => {
+                // Check if the response is correct
+                if (response) {
+                    msg.payload = response;
+                    node.send(msg);
+                }
+                // Catch the error and let the client know
+                else {
+                    // Send the error to the console as well
+                    node.error(err, msg);
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: err
+                    });
+                    // replace the payload with the actual error
+                    msg.payload = err.toString();
+                }
+            });
+        });
+    }
+    RED.nodes.registerType("getNextScheduledHardfork", getNextScheduledHardforkNode);
+  }
---
+++ nodes/globals/getRewardFund.js
@@ -0,0 +1,49 @@
+var steem = require('steem');
+var validateParams = require('../util/validateFields');
+
+module.exports = (RED) => {
+    "use strict";
+    function getRewardFundNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        
+        node.on('input', (msg) => {
+
+            // Set initial status of the node
+            node.status({});
+
+            // Pass arguments to validator
+            let validation = validateParams(param.accountName);
+
+            if (validation === false) {
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "All params are required"
+                });
+                return false;
+            }
+
+            steem.api.getRewardFund(param.accountName, (err, response) => {
+                // Check if the response is correct
+                if (response) {
+                    msg.payload = response;
+                    node.send(msg);
+                }
+                // Catch the error and let the client know
+                else {
+                    // Send the error to the console as well
+                    node.error(err, msg);
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: err
+                    });
+                    // replace the payload with the actual error
+                    msg.payload = err.toString();
+                }
+            });
+        });
+    }
+    RED.nodes.registerType("getRewardFund", getRewardFundNode);
+  }
