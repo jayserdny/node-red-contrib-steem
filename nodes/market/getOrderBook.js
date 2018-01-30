8ca8def99718398aa2d83ef47bef8f4994d82c17
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-01-30 00:52:10 -0500

    Market nodes added and force some nodes to have required parameters

---
+++ nodes/market/getOrderBook.js
@@ -0,0 +1,52 @@
+var steem = require('steem');
+var validateParams = require('../util/validateFields');
+
+module.exports = (RED) => {
+    "use strict";
+    function getOrderBookNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        var param = config;
+        
+        node.on('input', (msg) => {
+
+            // Set initial status of the node
+            node.status({});
+
+            // Pass arguments to validator
+            let validation = validateParams(param.limit);
+
+            if (validation === false) {
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "All params are required"
+                });
+                return false;
+            }
+
+            let limit = parseInt(param.limit.trim());
+
+            steem.api.getOrderBook(limit, (err, response) => {
+                // Check if the response is correct
+                if (response) {
+                    msg.payload = response;
+                    node.send(msg);
+                }
+                // Catch the error and let the client know
+                else {
+                    // Send the error to the console as well
+                    node.error(err, msg);
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: err
+                    });
+                    // replace the payload with the actual error
+                    msg.payload = err.toString();
+                }
+            });
+        });
+    }
+    RED.nodes.registerType("getOrderBook", getOrderBookNode);
+  }
