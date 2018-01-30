3d1d0684aaedf02bd8b3d8248bb7e9f62ea1123f
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-01-30 00:55:12 -0500

    Versioning to 0.0.4 and keys nodes added

---
+++ nodes/keys/getKeyReferences.js
@@ -0,0 +1,52 @@
+var steem = require('steem');
+var validateParams = require('../util/validateFields');
+
+module.exports = (RED) => {
+    "use strict";
+    function getKeyReferencesNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        var param = config;
+        
+        node.on('input', (msg) => {
+
+            // Set initial status of the node
+            node.status({});
+
+            // Pass arguments to validator
+            let validation = validateParams(param.key);
+
+            if (validation === false) {
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "All params are required"
+                });
+                return false;
+            }
+
+            let key = param.key.trim();
+
+            steem.api.getKeyReferences(key, (err, response) => {
+                // Check if the response is correct
+                if (response) {
+                    msg.payload = response;
+                    node.send(msg);
+                }
+                // Catch the error and let the client know
+                else {
+                    // Send the error to the console as well
+                    node.error(err, msg);
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: err
+                    });
+                    // replace the payload with the actual error
+                    msg.payload = err.toString();
+                }
+            });
+        });
+    }
+    RED.nodes.registerType("getKeyReferences", getKeyReferencesNode);
+  }
