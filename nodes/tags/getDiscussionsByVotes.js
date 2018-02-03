f32fd2d00d8128d8a3ba1dd8760f5766f6c50900
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-02-02 19:57:00 -0500

    js files for tags nodes added

---
+++ nodes/tags/getDiscussionsByVotes.js
@@ -0,0 +1,58 @@
+var steem = require('steem');
+var validateParams = require('../util/validateFields');
+
+module.exports = (RED) => {
+    "use strict";
+    function getDiscussionsByVotesNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        var param = config;
+        
+        node.on('input', (msg) => {
+
+            // Set initial status of the node
+            node.status({});
+
+            // Pass arguments to validator
+            let validation = validateParams(param.tag, param.limit);
+
+            if (validation === false) {
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "All params are required"
+                });
+                return false;
+            }
+
+            let tag = param.tag.trim();
+            let limit = param.limit.trim().toString();
+
+            let query = {
+                "tag": tag,
+                "limit": limit
+            };
+
+            steem.api.getDiscussionsByVotes(JSON.stringify(query), (err, response) => {
+                // Check if the response is correct
+                if (response) {
+                    msg.payload = response;
+                    node.send(msg);
+                }
+                // Catch the error and let the client know
+                else {
+                    // Send the error to the console as well
+                    node.error(err, msg);
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: err
+                    });
+                    // replace the payload with the actual error
+                    msg.payload = err.toString();
+                }
+            });
+        });
+    }
+    RED.nodes.registerType("getDiscussionsByVotes", getDiscussionsByVotesNode);
+  }
---
+++ nodes/tags/getTrendingTags.js
@@ -0,0 +1,53 @@
+var steem = require('steem');
+var validateParams = require('../util/validateFields');
+
+module.exports = (RED) => {
+    "use strict";
+    function getTrendingTagsNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        var param = config;
+        
+        node.on('input', (msg) => {
+
+            // Set initial status of the node
+            node.status({});
+
+            // Pass arguments to validator
+            let validation = validateParams(param.afterTag, param.limit);
+
+            if (validation === false) {
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "All params are required"
+                });
+                return false;
+            }
+
+            let afterTag = param.afterTag.trim();
+            let limit = ParseInt(param.limit.trim());
+
+            steem.api.getTrendingTags(afterTag, limit, (err, response) => {
+                // Check if the response is correct
+                if (response) {
+                    msg.payload = response;
+                    node.send(msg);
+                }
+                // Catch the error and let the client know
+                else {
+                    // Send the error to the console as well
+                    node.error(err, msg);
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: err
+                    });
+                    // replace the payload with the actual error
+                    msg.payload = err.toString();
+                }
+            });
+        });
+    }
+    RED.nodes.registerType("getTrendingTags", getTrendingTagsNode);
+  }
